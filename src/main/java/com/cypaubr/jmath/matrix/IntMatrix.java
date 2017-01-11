414876b7ce12921f1a7d20688b5477c47a346430
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2017-01-12 00:29:11 +0100

    Adding getters to IntMatrix class

--- src/main/java/com/cypaubr/jmath/matrix/IntMatrix.java
+++ src/main/java/com/cypaubr/jmath/matrix/IntMatrix.java
@@ -13,9 +13,45 @@ public class IntMatrix {
         this.matrix = matrix;
     }
 
+    /**
+     * Returns the length of a row
+     * @return int
+     */
+    public int rowLength(){
+        return matrix[0].length;
+    }
+
+    /**
+     * Returns the length of a column
+     * @return int
+     */
+    public int columnLength(){
+        return matrix.length;
+    }
+
+    /**
+     * Returns the number of elements in the matrix
+     * @return int
+     */
+    public int length(){
+        return matrix.length * matrix[0].length;
+    }
+
+    /**
+     * This method returns the matrix as a visual array
+     * @return String
+     */
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        //TODO
+        for (int i=0; i<matrix.length; i++){
+            sb.append('|');
+            for (int j=0; j<matrix[i].length; j++){
+                sb.append(matrix[i][j]);
+                sb.append(',');
+            }
+            sb.append('|');
+            sb.append("\r\n");
+        }
         return sb.toString();
     }
 }
--- src/test/java/IntMatrixTest.java
+++ src/test/java/IntMatrixTest.java
@@ -9,8 +9,35 @@ import static org.junit.Assert.assertEquals;
  * @version 1.0
  */
 public class IntMatrixTest {
+
+    private IntMatrix matrix;
+    private int tab[][] = {
+            {1,2,3},
+            {4,5,6}
+    };
+
+    @Before
+    public void initialize(){
+        matrix = new IntMatrix(tab);
+    }
+
     @Test
     public void InstanciationTest1(){
         //TODO
+    }
+
+    @Test
+    public void LengthTest(){
+        assertEquals("Should return 6",6,matrix.length());
+    }
+
+    @Test
+    public void RowLengthTest(){
+        assertEquals("Should return 3",3,matrix.rowLength());
+    }
+
+    @Test
+    public void ColumnLengthTest(){
+        assertEquals("Should return 2",2,matrix.columnLength());
     }
 }
